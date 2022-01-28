856ef40660083ea5de3fa9e30227bbf29ecbae0f
terayeah <harrypoppopoo@gmail.com>
2022-01-28 13:18:11 +0900

    .

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <Tutorial/>
+  <Tutorial/>
+  <Tutorial/>
+</div>
+</template>
+
+<script>
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+export default {
+  mounted(){
+    gsap.registerPlugin(ScrollTrigger);
+  }
+}
+</script>
